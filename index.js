4b24451b9cea36a972b9285b40ae14f4faa0c897
GAURANG-21 <gaurang2621@gmail.com>
2023-06-17 16:12:39 +0530

    simon

---
+++ index.js
@@ -0,0 +1,53 @@
+var interval;
+let c=1,t=0;
+let arr=[];
+let color=["green","red", "yellow","blue"];
+
+function numberGen(){
+    $("h1").text("Level "+c);
+    let n=Math.floor(4*Math.random());
+    if(t!=0 && arr[t-1]==n)
+    numberGen();
+    arr.push(n);
+    glow(n);
+}
+
+function glow(n){
+    $("div."+color[n]).addClass("pressed");
+    setTimeout(()=>{$("div."+color[n]).removeClass("pressed");},200);
+}
+
+function wrong(){
+    interval=setInterval(()=>{
+        $("body").toggleClass("game-over");
+    },100);
+    arr=arr.splice();
+    $("h1").text("Game over. Press any key to Restart");
+}
+
+document.addEventListener("click",()=>{
+    if(color[arr[t]]==event.target.id)
+    {
+        glow(arr[t]);
+        t++;
+        if(t==arr.length)
+        {
+            t=0;
+            c++;
+            setTimeout(numberGen, 1000);
+        }
+    }
+    else
+    {
+        c=1;
+        t=0;
+        wrong();
+    }
+    
+});
+document.addEventListener("keypress",()=>{
+    clearInterval(interval);
+    $("body").removeClass("game-over");
+    $("h1").text("Level "+c);
+    setTimeout(()=>{numberGen()},400);
+});
